e525c79fc9ff5b9780ee0f61dd8811b8f96fb408
quqxui <41822324+quqxui@users.noreply.github.com>
2025-01-08 11:39:47 +0800

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # webqsp
-CUDA_VISIBLE_DEVICES=0 bash run_ft.sh WebQSP LLaMA-2-7b-hf webqsp_64_7_32_16 train 64 7 0 32 16 15
+CUDA_VISIBLE_DEVICES=0 bash run_ft.sh WebQSP LLaMA-2-7b-hf webqsp_100_7_32_16 train 100 7 0 32 16 15
 sleep 1m
-CUDA_VISIBLE_DEVICES=0 bash run_ft.sh WebQSP LLaMA-2-7b-hf webqsp_64_7_32_16 test 64 7 0 32 16 15
-CUDA_VISIBLE_DEVICES=0 python -u eval_final.py --dataset WebQSP --pred_file Reading/LLaMA-2-7b-hf/WebQSP_webqsp_64_7_32_16/evaluation_beam/beam_test_top_k_predictions.json
-CUDA_VISIBLE_DEVICES=0 python -u eval_final.py --dataset WebQSP --pred_file Reading/LLaMA-2-7b-hf/WebQSP_webqsp_64_7_32_16/evaluation_beam/beam_test_top_k_predictions.json --golden_ent
+CUDA_VISIBLE_DEVICES=0 bash run_ft.sh WebQSP LLaMA-2-7b-hf webqsp_100_7_32_16 test 100 7 0 32 16 15
+CUDA_VISIBLE_DEVICES=0 python -u eval_final.py --dataset WebQSP --pred_file Reading/LLaMA-2-7b-hf/WebQSP_webqsp_100_7_32_16/evaluation_beam/beam_test_top_k_predictions.json
+CUDA_VISIBLE_DEVICES=0 python -u eval_final.py --dataset WebQSP --pred_file Reading/LLaMA-2-7b-hf/WebQSP_webqsp_100_7_32_16/evaluation_beam/beam_test_top_k_predictions.json --golden_ent
 
 
 # cwq
